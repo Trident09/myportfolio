15ba38e4a350235d9014c153247b91a079e55642
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-24 09:18:34 +0000

    minor changes and page setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;

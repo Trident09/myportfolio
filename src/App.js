98122a1637c6ba98b5f3b80f889579799e2c995c
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-24 13:48:00 +0000

    all individual pages added (blank)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route index element={<Home />} />
+			<Route
+				path="/"
+				element={<Layout />}
+			>
+				<Route
+					index
+					element={<Home />}
+				/>
+				<Route
+					path="/about"
+					element={<AboutMe />}
+				/>
+				<Route
+					path="/services"
+					element={<Services />}
+				/>
+				<Route
+					path="/portfolio"
+					element={<Portfolio />}
+				/>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
 			</Route>
 		</Routes>
 	);

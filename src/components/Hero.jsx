cab3e80fd83a4fa4f5604b0e3309a1a53847a56e
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-25 11:14:25 +0000

    added a scroll animation to the hero section and changes the button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../css/hero-scroll.css";
+import "../css/hero-button.css";
 
 function Hero() {
 	return (
@@ -9,19 +11,33 @@ function Hero() {
 			<div class="w-full py-16 px-10 md:px-20 min-h-screen flex flex-col items-start justify-center">
 				<h1 class="text-4xl font-bold leading-loose">
 					Hi, my name is{" "}
-					<span class="font-extrabold text-6xl text-cyan-500 md:flex-col">RUPAM <b className="opacity-95 text-cyan-300 text-5xl">BARUI</b></span>
+					<span class="font-extrabold text-6xl text-cyan-500 md:flex-col">
+						RUPAM{" "}
+						<b className="opacity-95 text-cyan-300 text-5xl">
+							BARUI
+						</b>
+					</span>
 					<br />
-					<span>I'm the <b className="text-cyan-300">Software Developer.</b></span>
+					<span>
+						I'm the{" "}
+						<b className="text-cyan-300">Software Developer.</b>
+					</span>
 				</h1>
-				<p class="text-white text-lg uppercase border-2 p-4 my-7 hover:bg-cyan-300 hover:text-black hover:border-black transition-colors">
-					<a
-						rel="noreferrer"
-						class="cta-btn cta-btn--hero"
-						href="#about"
-					>
-						Know more
-					</a>
-				</p>
+				<div className="flex flex-row gap-4 justify-center items-center">
+						<a
+							rel="noreferrer"
+							href="#about"
+						>
+							<button class="button"> KNOW MORE
+							</button>
+						</a>
+					<div class="scrolldown scale-75">
+    					<div class="chevrons">
+        					<div class="chevrondown"></div>
+        					<div class="chevrondown"></div>
+    					</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);

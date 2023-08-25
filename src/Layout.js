f9058918e4885f157c1fe330f96454229aa39ac4
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-25 10:05:51 +0000

    added footer with email input, socials and a link to contaxt page

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 
 export default function Layout() {
     return (

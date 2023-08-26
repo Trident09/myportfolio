9ffbe59b14ac257d93db95fd4d2629aefd862cc0
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-26 17:48:41 +0000

    added working preloader

--- src/Layout.js
+++ src/Layout.js
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/footer/Footer";
 import PreLoader from "./components/PreLoader";
 
 export default function Layout() {
-	return (
-		<>
-			<body>
-				{/* <PreLoader /> */}
-				<NavBar />
-				<main className="mt-20">
-					<Outlet />
-				</main>
-				<Footer />
-			</body>
-		</>
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const fakeDataFetch = () => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 1500);
+		};
+		fakeDataFetch();
+	}, []);
+	return isLoading ? (
+		<PreLoader />
+	) : (
+		<body>
+			<NavBar />
+			<main className="mt-20">
+				<Outlet />
+			</main>
+			<Footer />
+		</body>
 	);
 }

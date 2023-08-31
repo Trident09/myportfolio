de045658c9b0a8b9a3f40f3d32ebb2039d937dc7
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-31 12:32:27 +0000

    Project Page

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -8,14 +8,16 @@ function ProjectSection() {
 	return (
 		<section
 			id="portfolio"
-			className="py-16 px-10 md:px-20 w-full min-h-screen bg-blue-300 text-blue-900 flex justify-center items-center"
+			className="py-16 px-10 md:px-20 w-full min-h-screen bg-blue-300 text-blue-900 flex flex-col justify-center items-center"
 		>
 			<div
 				class="section-portfolio py-4 px-0"
 				id="portfolio-section"
 			>
 				<div class="my-0 py-0">
-					<h2 class="common-heading text-3xl font-semibold capitalize relative leading-normal mb-5 w-max">LATEST WORKS</h2>
+					<h2 class="common-heading text-3xl font-semibold capitalize relative leading-normal mb-5 w-max">
+						LATEST WORKS
+					</h2>
 					<p className="text-[#777] text-xl leading-relaxed w-[85%] tracking-wide">
 						I like to work on small applications which help me in
 						improving my knowledge about the topic. Here are a few
@@ -27,8 +29,8 @@ function ProjectSection() {
 				<div class="flex justify-center items-center mt-8 mb-0 w-full">
 					<AboutButton Texts={PortfolioBtns} />
 				</div>
-				<ProjectCard Projects={Portfolios} />
 			</div>
+			<ProjectCard Projects={Portfolios} />
 		</section>
 	);
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { PortfoliosAll } from "../Assets/Menus";
+import ProjectCard from "../components/ProjectCard";
+import "../css/project-section.css";
 
 function Portfolio() {
-  return (
-    <div className='min-h-screen w-full bg-cyan-200 flex justify-center items-center'>Portfolio</div>
-  )
+	return (
+		<section
+			id="portfolio"
+			className="py-16 px-10 md:px-20 w-full min-h-screen bg-blue-300 text-blue-900 flex flex-col justify-center items-center"
+		>
+			<div
+				class="section-portfolio py-4 px-0"
+				id="portfolio-section"
+			>
+				<div class="my-0 py-0">
+					<h2 class="common-heading text-3xl font-semibold capitalize relative leading-normal mb-5 w-max">
+						All Projects That I've Worked On
+					</h2>
+					<p className="text-[#777] text-xl leading-relaxed w-[85%] tracking-wide">
+						I like to work on small applications which help me in
+						improving my knowledge about the topic. Here are a few
+						design projects I've worked on recently. Want to see
+						more? Visit the full page below.
+					</p>
+				</div>
+			</div>
+			<ProjectCard Projects={PortfoliosAll} />
+		</section>
+	);
 }
 
-export default Portfolio
+export default Portfolio;

dfb37f0cab12610d244d3e97e2b58fd6616c8c7d
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-10-15 09:13:26 +0530

    Extraction File for TailwindCSS

---
+++ src/css/stylesheet.js
@@ -0,0 +1,54 @@
+export const AboutButton = {
+}
+
+export const AboutSection = {
+}
+
+export const Contact = {
+}
+
+export const Hero = {
+}
+
+export const HeroButton = {
+}
+
+export const LeftSkewDown = {
+}
+
+export const LoadingAnimation = {
+}
+
+export const NavBar = {
+}
+
+export const PreLoader = {
+}
+
+export const ProjectCard = {
+}
+
+export const projectSection = {
+}
+
+export const ResourceSection = {
+}
+
+export const ScrollAnimation = {
+}
+
+export const AboutMePage = {
+}
+
+export const ContactPage = {
+}
+
+export const HomePage = {
+}
+
+export const PortfolioPage = {
+}
+
+export const ServicesPage = {
+}
+
